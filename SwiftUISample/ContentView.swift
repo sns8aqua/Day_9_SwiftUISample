ea10b6b3fd5fc6e4e117760cdc9045b75d10093c
SathyaNarayan <roflmeatu@gmail.com>
2022-01-24 23:43:52 +0100

    ContentView

--- SwiftUISample/ContentView.swift
+++ SwiftUISample/ContentView.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var userName: String = ""
+    @State private var password: String = ""
+    
+    @State private var selectedColor = 0
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        List(0 ..< 5) { item in
+//            Image(systemName: "photo")
+//            Text("Text \(item)")
+//        }
+        
+        
+        Picker("Pick your Color", selection: $selectedColor) {
+            Text("Red").tag(0)
+            Text("Green").tag(1)
+            Text("Blue").tag(2)
+        }
+        
+        
+        Text("Value:\(selectedColor)")
+        
+        
+        
+        
+        
+//        VStack(alignment: .center, spacing: 10, content: {
+//
+//            Group {
+//                TextField("Email", text: $userName)
+//                    .frame(width: 300, height: 50, alignment: .center)
+//                    .background(Color.red)
+//                    .padding()
+//                SecureField("Password", text: $password)
+//                    .frame(width: 300, height: 50, alignment: .center)
+//                    .background(Color.red)
+//                    .padding()
+//
+//            }.frame(alignment: .top)
+//
+//            Group {
+//                Button( action: {
+//
+//                    print("Clicked")
+//
+//                    print(userName)
+//                    print(password)
+//
+//                }, label: {
+//                    Text("Login")
+//                })
+//                .frame(width: 200, height: 50, alignment: .center)
+//                .background(Color.blue)
+//                .foregroundColor(.white)
+//                .font(.system(size: 18))
+//
+//            }.frame(alignment: .bottom)
+//
+//
+//
+//
+//
+//
+//
+//        }).padding()
+        
+        
+        
+        
     }
 }
 
